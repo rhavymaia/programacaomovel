5ddeaf4a8f38db5c2463315dfe3de3677ac0618a
rhavymaia <rmg3@cin.ufpe.br>
2016-03-05 11:26:53 -0300

    Splash
    - Splash com mudança de tela via Handle.

--- EditTextListenerApp/app/src/main/java/edittextlistener/ifpb/edu/br/edittextlistenerapp/activity/SplashScreenActivity.java
+++ EditTextListenerApp/app/src/main/java/edittextlistener/ifpb/edu/br/edittextlistenerapp/activity/SplashScreenActivity.java
@@ -1,13 +1,30 @@
 package edittextlistener.ifpb.edu.br.edittextlistenerapp.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashScreenActivity extends AppCompatActivity {
+import edittextlistener.ifpb.edu.br.edittextlistenerapp.R;
+
+public class SplashScreenActivity extends Activity implements Runnable {
+
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(this, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void run() {
+        Intent intent = new Intent(this, BuscarNomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
